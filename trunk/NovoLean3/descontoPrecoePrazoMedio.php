76f023060c0fd65be2cd6cb63d2f69494cd693d3
wevertoncomp@gmail.com <wevertoncomp@gmail.com@7c1f461d-6f30-f74a-1002-a76c8745bbc2>
2015-02-18 00:41:25 +0000

    Criação de tela de desconto, prazo e preço médio

---
+++ trunk/NovoLean3/descontoPrecoePrazoMedio.php
@@ -0,0 +1,195 @@
+<?php
+// ob_end_flush();
+$conn->open ( $connStr );
+echo "<div class='well'>";
+echo "<h4>Desconto, Preço e Prazo Médio</h4><hr>";
+echo "</div>";
+
+echo "<div class='well'>";
+
+$retornaSQL = "	SELECT 
+
+					A3.A3_COD, A3.A3_NOME, A3.A3_NREDUZ, RTRIM(A3.A3_REGIAO)
+					
+					FROM SA3010 A3
+					
+					WHERE A3.D_E_L_E_T_ <> '*' 
+					AND A3.A3_MSBLQL <> '1'
+					
+					ORDER BY A3.A3_REGIAO, A3.A3_COD";
+
+$rs = $conn->execute ( $retornaSQL );
+
+$num_columns = $rs->Fields->Count ();
+
+for($i = 0; $i < $num_columns; $i ++) {
+	$fld [$i] = $rs->Fields ( $i );
+}
+
+$regiao2 = null;
+
+while ( ! $rs->EOF ) {
+	
+	$representante = $fld [0]->value;
+	$nome = $fld [1]->value;
+	$nomeReduzido = $fld [2]->value;
+	$regiao = $fld [3]->value;
+	
+	if (empty ( $regiao2 ) || ($regiao2 != $regiao)) {
+		echo "<h2>Região $regiao</h2>";
+	}
+	$regiao2 = $regiao;
+	
+	echo "<h3>$representante - $nome - $nomeReduzido</h3>";
+	
+		echo "<table class='table table-hover'><tr><th>Item</th><th>Mês</th><th>Ano</th><th>Filial</th>";
+		echo "<th>Cliente</th><th>Razão Social</th><th>Nota</th><th>Pedido</th><th>Emissão</th>";
+		echo "<th>Comissão</th><th>Prazo M</th><th>Desconto M</th><th>Itens</th><th>Val. Liq.</th><th>Vendedor</th></tr>";
+	
+	// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+	$retornaSQL2 = "SELECT 	DATEPART(mm, F.F2_EMISSAO) AS MES,
+					DATEPART(YY, F.F2_EMISSAO) AS ANO,
+					F.F2_FILIAL AS FILIAL,
+					F.F2_CLIENTE,
+					RTRIM(A.A1_NOME),
+					F.F2_DOC,
+					(SELECT TOP 1 D2_PEDIDO
+					 FROM SD2010 WITH (NOLOCK)
+					 WHERE 	D_E_L_E_T_ != '*'
+						AND D2_DOC = F.F2_DOC
+						AND D2_FILIAL = F.F2_FILIAL
+					) AS D2_PEDIDO,
+					F.F2_EMISSAO,
+					(SELECT TOP 1 D2_COMIS1
+					 FROM SD2010 WITH (NOLOCK)
+					 WHERE 	D_E_L_E_T_ != '*'
+						AND D2_DOC = F.F2_DOC
+						AND D2_FILIAL = F.F2_FILIAL
+					) AS D2_COMIS1,
+					E4.E4_XMEDPRA,
+					(SELECT ROUND(AVG(1-((1-(C6.C6_ZDESCON/100))*(1-(C6.C6_ZDESCO2/100)))),4)
+				 	 FROM SC6010 C6 WITH (NOLOCK)
+				 	 INNER JOIN SC5010 C5 ON C5.C5_NUM = C6.C6_NUM
+				 	 WHERE 	C6.C6_FILIAL = F.F2_FILIAL
+				 	 		AND C6.D_E_L_E_T_ != '*'
+				 	 		AND C5.D_E_L_E_T_ != '*'
+				 	 		AND (C5.C5_NOTA = F.F2_DOC OR (C5.C5_NOTA = F.F2_DOC AND C5.C5_NUM = C5.C5_XCOMPPE))
+					) AS DESCONTO_MEDIO,
+					(SELECT SUM(D2_QUANT)
+					 FROM SD2010 WITH (NOLOCK)
+					 WHERE 	D_E_L_E_T_ != '*'
+						AND D2_DOC = F.F2_DOC
+						AND D2_FILIAL = F.F2_FILIAL
+					) AS QTD_ITENS,
+					F.F2_VALBRUT,
+				   	F.F2_VALMERC AS VALOR_LIQUIDO,
+				   	F.F2_VEND1
+			
+			FROM SF2010 F WITH (NOLOCK)
+			
+			INNER JOIN SA1010 A ON F.F2_CLIENTE = A.A1_COD AND A.D_E_L_E_T_ != '*'
+			INNER JOIN SA3010 A3 ON A3.A3_COD = F.F2_VEND1 AND A3.D_E_L_E_T_ != '*'
+			LEFT OUTER JOIN SE4010 E4 ON E4.E4_FILIAL = F.F2_FILIAL AND E4.E4_CODIGO = F.F2_COND AND E4.D_E_L_E_T_ <> '*'
+			
+			WHERE 	(F.F2_FILIAL = '0101' OR F.F2_FILIAL = '0201')
+				AND F.F2_SERIE = '1'
+				AND (SELECT COUNT(E1_PARCELA) FROM SE1010 WITH (NOLOCK) WHERE E1_NUM = F.F2_DOC) > 0
+				AND F.F2_EMISSAO BETWEEN '20150101' AND '20150231'
+				AND F.D_E_L_E_T_ != '*'
+				AND F.F2_VEND1 = '$representante'
+				AND F.F2_CLIENTE <> '003718'
+				
+				ORDER BY F.F2_FILIAL, DATEPART(YY, F.F2_EMISSAO) DESC,
+				DATEPART(mm, F.F2_EMISSAO) ASC";
+	
+	$rs2 = $conn->execute ( $retornaSQL2 );
+	
+	$num_columns2 = $rs2->Fields->Count ();
+	
+	$item = 0;
+	$filial2 = null;
+	
+	$prazoMedioTotal = 0;
+	$descontoMedioTotal = 0;
+	$quantidadeItensTotal = 0;
+	$valorLiquidoTotal = 0;
+	
+	$prazoMedioXValorTotal = 0;
+	$descontoMedioXValorTotal = 0;
+	
+	
+	for($i2 = 0; $i2 < $num_columns2; $i2 ++) {
+		$fld2 [$i2] = $rs2->Fields ( $i2 );
+	}
+	while ( ! $rs2->EOF ) {
+		
+		$item ++;
+		$mes = $fld2 [0]->value;
+		$ano = $fld2 [1]->value;
+		$filial = $fld2 [2]->value;
+		$cliente = $fld2 [3]->value;
+		$razaoSocial = $fld2 [4]->value;
+		$nota = $fld2 [5]->value;
+		$pedido = $fld2 [6]->value;
+		$emissao = $fld2 [7]->value;
+		$comissao = $fld2 [8]->value;
+		$prazoMedio = $fld2 [9]->value;
+		$descontoMedio = $fld2 [10]->value;
+		$quantidadeItens = $fld2 [11]->value;
+		$valorBruto = $fld2 [12]->value;
+		$valorLiquido = $fld2 [13]->value;
+		$vendedor = $fld2 [14]->value;
+		
+		$prazoMedioTotal += $prazoMedio;
+		$descontoMedioTotal += $descontoMedio;
+		$quantidadeItensTotal += $quantidadeItens;
+		$valorLiquidoTotal += $valorLiquido;
+		
+		$prazoMedioXValorTotal += $prazoMedio * $valorLiquido;
+		$descontoMedioXValorTotal += $descontoMedio * $valorLiquido;
+		
+		if (empty ( $filial2 ) || ($filial2 != $filial)) {
+			if ($filial == '0101') {
+				echo "Pradolux";
+			} else {
+				echo "Luxparts";
+			}
+		}
+		$filial2 = $filial;
+		
+		echo "<tr><td>$item</td><td>$mes</td><td>$ano</td><td>$filial</td><td>$cliente</td><td>$razaoSocial</td>";
+		echo "<td>$nota</td><td>$pedido</td><td>$emissao</td><td>$comissao</td><td>$prazoMedio</td><td>" . number_format ( $descontoMedio * 100, 2, ',', '.' ) . "</td>";
+		echo "<td>$quantidadeItens</td><td>$valorLiquido</td><td>$vendedor</td></tr>";
+		$rs2->MoveNext ();
+	}
+	
+	echo "<tr><th colspan = '10'>Médias Simples</th><th>".number_format(($prazoMedioTotal/$item), 2, ',', '.')."</th>";
+	echo "<th>".number_format((($descontoMedioTotal/$item)*100), 2, ',', '.')."</th>";
+	echo "<th>".number_format(($quantidadeItensTotal/$item), 0, ',', '.')."</th>";
+	echo "<th>".number_format(($valorLiquidoTotal/$item), 0, ',', '.')."</th><th></th></tr>";
+	
+	echo "<tr><th colspan = '10'>Médias Ponderadas</th><th>".number_format(($prazoMedioXValorTotal/$valorLiquidoTotal), 2, ',', '.')."</th>";
+	echo "<th>".number_format((($descontoMedioXValorTotal/$valorLiquidoTotal)*100), 2, ',', '.')."</th>";
+	echo "<th></th>";
+	echo "<th></th><th></th></tr>";
+	
+	echo "</tr></table>";
+	
+	// $rs2->MoveNext ();
+	$rs2->Close ();
+	$rs2 = null;
+	// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+	
+	$rs->MoveNext ();
+}
+
+// $rs->MoveNext ();
+
+echo "</div>";
+
+$rs->Close ();
+$rs = null;
+
+$conn->Close ();
+$conn = null;
+?>
