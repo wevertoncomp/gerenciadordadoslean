dbd314e921a5b1a8fd02aac40d2d05222eeaa0d3
obsilva94@gmail.com <obsilva94@gmail.com@7c1f461d-6f30-f74a-1002-a76c8745bbc2>
2015-04-28 20:47:49 +0000

    Testes iniciais

--- trunk/NovoLean3/area_restrita/logar.php
+++ trunk/NovoLean3/area_restrita/logar.php
@@ -1,4 +1,5 @@
 <?php
+$conn->open ( $connStr );
 session_start();
 
 ob_start();
@@ -6,21 +7,48 @@ ob_start();
 include "../conexao.php";
 
 $login = $_POST['login'];
-$senha = $_POST['senha'];
+//$senha = $_POST['senha'];
 
-$sql = mysql_query("SELECT * FROM st_usuarios WHERE login = '$login' AND senha = '".md5($senha)."' AND (nivel = 3 OR nivel = 1) AND liberado = 1");
+/*$sql = mysql_query("SELECT * FROM st_usuarios WHERE login = '$login'
+		--AND senha = '".md5($senha)."' AND (nivel = 3 OR nivel = 1) AND liberado = 1
+		");*/
 
-	if(mysql_num_rows($sql) == 1)
-	{
-		$_SESSION['login_session'] = $login;
-		$_SESSION['senha_session'] = $senha;
-		header("location: pagina2.php");
-		include "pagina2.php";
-	}else{
-		unset($_SESSION['login_session']);
-		unset($_SESSION['senha_session']);
-		include "index.php";
-	}
+$retornaSQL = "SELECT RA.RA_MAT
+				FROM SRA010 RA
+				WHERE RA.RA_DEMISSA = '' AND RA.RA_CATFUNC = 'M' AND RA.RA_MAT = RA.RA_XENCAR
+				AND RA.RA_MAT = '$login'";
+
+$rs = $conn->execute ( $retornaSQL );
+$num_columns = $rs->Fields->Count ();
+
+for($i = 0; $i < $num_columns; $i ++) {
+	$fld1 [$i] = $rs->Fields ( $i );
+}
+
+while ( ! $rs->EOF ) {
+ $login = $fld1 [0]->value;
+$rs->MoveNext ();
+}
+
+if(isset ($login))
+{
+	$_SESSION['login_session'] = $login;
+	//$_SESSION['senha_session'] = $senha;
+	header("location: pagina2.php");
+	include "pagina2.php";
+}else{
+	unset($_SESSION['login_session']);
+	//unset($_SESSION['senha_session']);
+	include "index.php";
+}
+/*while ( ! $rs->EOF ) {
+	$usuario = $fld1 [0]->value;
+	$rs->MoveNext ();
+}*/
+$rs->Close ();
+$rs = null;
+
+
 	
 	ob_end_flush();
 ?>
