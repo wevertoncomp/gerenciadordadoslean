6f5979237a02d193ebcbddcbf9b5c96dfe13472a
obsilva94 <obsilva94@gmail.com>
2015-07-13 18:00:21 +0000

    Inserção de Kanban

--- NovoLean3/produtoEmbalagem.php
+++ NovoLean3/produtoEmbalagem.php
@@ -12,12 +12,19 @@ echo "</div>";
 	echo "<table class='table table-hover'>";
 	echo "<tr>";
 	echo "<th>Produto</th>";
-	echo "<th>Qtd. Embalagem</th>";
+	echo "<th>Qtd. Emb.</th>";
 	echo "<th>Caixa Produto</th>";
 	echo "<th>Caixa Estrut.</th>";
 	echo "<th>Local</th>";
 	echo "<th>Saquinho</th>";
 	echo "<th>Descrição Saquinho</th>";
+	echo "<th>Local Kanban</th>";
+	echo "<th>VD</th>";
+	echo "<th>AM</th>";
+	echo "<th>VM</th>";
+	echo "<th>Por Kanban</th>";
+	echo "<th>Total</th>";
+	echo "<th>Caixas</th>";
 	echo "</tr>";
 	
 	$retornaSQL = "SELECT RTRIM(B1.B1_COD),
@@ -26,9 +33,14 @@ echo "</div>";
 					RTRIM(G1_1.G1_COMP),
 					RTRIM(B1.B1_LOCPAD),
 					RTRIM(G1.G1_COMP),
-					RTRIM(B1_1.B1_DESC)
+					RTRIM(B1_1.B1_DESC),
+					RTRIM(Z9.ZZ9_LOCAL),
+					RTRIM(Z9.ZZ9_KANBVD),
+					RTRIM(Z9.ZZ9_KANBAM),
+					RTRIM(Z9.ZZ9_KANBVM),
+					RTRIM(Z9.ZZ9_QTDKAN)
 					
-					FROM SB1010 B1
+					FROM SB1010 B1 WITH (NOLOCK)
 						LEFT OUTER JOIN SB5010 B5
 							ON B1.B1_COD = B5.B5_COD
 						LEFT OUTER JOIN SG1010 G1
@@ -42,6 +54,9 @@ echo "</div>";
 							ON B1.B1_COD = G1_1.G1_COD
 							AND G1_1.G1_COMP LIKE 'KIT-CAIXA%'
 							AND G1_1.D_E_L_E_T_ <> '*'
+						LEFT OUTER JOIN ZZ9010 Z9
+							ON B1.B1_COD = Z9.ZZ9_PRODUT
+							AND Z9.D_E_L_E_T_ <> '*'
 							
 					WHERE B1.D_E_L_E_T_ <> '*'
 						AND B1.B1_COD LIKE 'PL________'
@@ -70,18 +85,38 @@ echo "</div>";
 		$armazem 			= $fld [4]->value;
 		$codigoSaquinho 	= $fld [5]->value;
 		$descricaoSaquinho 	= $fld [6]->value;
+		$localKanban 		= $fld [7]->value;
+		$kanbanVD 			= $fld [8]->value;
+		$kanbanAM 			= $fld [9]->value;
+		$kanbanVM			= $fld [10]->value;
+		$qtdPorKanban 		= $fld [11]->value;
 		
+		$total = NULL;
+		$caixasNecessarias = NULL;
+		
+		$total = ($kanbanVD + $kanbanAM + $kanbanVM) * $qtdPorKanban;
+		$caixasNecessarias = ($total / $qtdEmbalagem);
+		
+
 		$caixaProd = substr($caixaProduto, 0, 3);
 		$caixaEstrut = substr($caixaEstrutura, 9, 12);
+		
  		if ($caixaProd != $caixaEstrut) {
  			$corCaixa = "#FF8C00";
  		} else {
  			$corCaixa = NULL;
  		}
+ 		
  		if (empty($codigoSaquinho)) {
  			$corSaquinho = "#FF8C00";
  		} else {
  			$corSaquinho = NULL;
+ 		}
+ 		
+ 		if ($armazem != $localKanban) {
+ 			$corVerificacaoKanban = "#FF8C00";
+ 		} else {
+ 			$corVerificacaoKanban = NULL;
  		}
 		
 		echo "<tr>";
@@ -92,11 +127,18 @@ echo "</div>";
 		echo "<td>$armazem</td>";
 		echo "<td bgcolor = '$corSaquinho'>$codigoSaquinho</td>";
 		echo "<td bgcolor = '$corSaquinho'>$descricaoSaquinho</td>";
+		echo "<td bgcolor = '$corVerificacaoKanban'>$localKanban</td>";
+		echo "<td>$kanbanVD</td>";
+		echo "<td>$kanbanAM</td>";
+		echo "<td>$kanbanVM</td>";
+		echo "<td>$qtdPorKanban</td>";
+		echo "<td>$total</td>";
+		echo "<td>$caixasNecessarias</td>";
 		echo "</tr>";
 		$rs->MoveNext ();
 	}
 	
-	$rs->MoveNext ();
+	//$rs->MoveNext ();
 
 echo "</tr></table>";
 echo "</div>";
